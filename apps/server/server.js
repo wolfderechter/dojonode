ee80e131eaa169324f2c1c488af6fd5369253aa5
wolfez <wolfderechter@gmail.com>
2025-02-05 21:21:26 +0100

    linting + mock gasPrice endpoint

--- apps/server/server.js
+++ apps/server/server.js
@@ -1,32 +1,36 @@
-const express = require('express');
-const si = require('systeminformation');
-const cors = require('cors');
+const express = require("express");
+const si = require("systeminformation");
+const cors = require("cors");
 const app = express();
 const port = 3009;
 
 // Start new timer on startup, to keep track of runtime
 const startTime = Math.floor(new Date() / 1000);
 
-app.use(cors({
-  origin: '*'
-}));
-app.get('/metrics', async (req, res) => {
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.get("/metrics", async (_req, res) => {
   const mem = await si.mem();
   const cpu = await si.currentLoad();
   const disk = await si.fsSize();
-
-  // const dockerContainers = await si.dockerContainers();
-  // const docker = dockerContainers.find(dc => dc.name === "simple-taiko-node-taiko_client_driver-1");
 
   const metrics = {
     mem,
     cpu,
     disk,
-    startTime
+    startTime,
   };
 
   res.json(metrics);
+});
 
+// TODO: Send request to the node
+app.get("/api/gasPrice", async (req, res) => {
+  res.json("5");
 });
 
 app.listen(port, () => {
