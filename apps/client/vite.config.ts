d1e3a85c103a025096e4a95a8121dfb21dd6805f
wolfez <wolfderechter@gmail.com>
2025-02-15 20:49:30 +0100

    cleanup

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { VitePWA } from "vite-plugin-pwa";
 import { sveltePreprocess } from "svelte-preprocess";
-import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
