94d469e8c3646a4e6704aacc812ccec7a9c894bd
wolfez <wolfderechter@gmail.com>
2025-02-25 20:25:33 +0100

    refactor: use fs to read/write file instead of Bun

--- apps/server/utils/helpers.ts
+++ apps/server/utils/helpers.ts
@@ -1,20 +1,23 @@
 import { Config } from "./types";
+import fs from "fs/promises";
 
 export async function readConfig(): Promise<Config> {
-  const defaultConfig: Config = { NODE_API_URL: "http://localhost:8545"};
-  try{
-    const config = await Bun.file("./config.json", { type: "application/json" }).json();
-    return {...defaultConfig, ...config}; // default NODE_API_URL will be overriden by the NODE_API_URL from config
-  } catch(error){
+  const defaultConfig: Config = { NODE_API_URL: "http://localhost:8545" };
+  try {
+    const config = await fs.readFile("./config.json", "utf8");
+    console.log("read config:", config);
+    return { ...defaultConfig, ...JSON.parse(config) }; // default NODE_API_URL will be overriden by the NODE_API_URL from config
+  } catch (error) {
     console.error("Failed to read config file");
     writeConfig(defaultConfig);
     return defaultConfig;
   }
 }
-export async function writeConfig(config: Config){
-  try{
-    await Bun.write("./config.json", JSON.stringify(config));
-  } catch(error){
+export async function writeConfig(config: Config) {
+  try {
+    await fs.writeFile("./config.json", JSON.stringify(config));
+    console.log("wrote config:", config);
+  } catch (error) {
     console.error("Failed to write config file");
   }
 }
